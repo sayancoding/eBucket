6d8409525780dd8b9f598c98ac37bedaa92ea6b8
SAYAN MAITY <sayanmaity007@gmail.com>
2019-10-21 20:46:36 +0530

    Header Comp

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import {Route} from 'react-router-dom';
 import HomeArea from "./Components/HomeArea/homeArea";
 import ShopingArea from './Components/ShopingArea/ShopingHome/shopingHome'
 import Item_preview from './Containers/Item_Preview/Item_preview'
+import Haeder from './Components/Header/Header'
+
 import SignIn from './Containers/SignIn/SignIn'
 import {auth,createUserProfileDocuments} from './Firebase/firebase.config'
+import Header from './Components/Header/Header';
 
 
 class App extends Component {
@@ -50,12 +53,16 @@ class App extends Component {
   render() {
      
     return (
-      <div className="App">
+      <div>
+        <Header/>
+        
+        <div className="App">
         <h1>Welcome</h1>
             <Route path='/' exact component={HomeArea} />
             <Route path='/shop' exact component={ShopingArea} />
             <Route path='/shop/:ItemId' exact component={Item_preview} />
             <Route path='/signin' exact component={SignIn} />
+        </div>
       </div>
     )
   }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+
+const Header = (props) => {
+    return (
+        <div>
+            
+            <nav>
+                <div className="imgArea">
+                    <img src="" alt=""/>
+                </div>
+                
+                <div className="option">
+                    <ul>
+                        <li>Home</li>
+                        <li>Shop</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Header;
